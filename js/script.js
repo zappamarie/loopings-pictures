2853b731f50a605afba3416c5ea57d1e782b514d
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2018-02-21 21:10:42 +0100

    add swag slider

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: $('.gallery-top .swiper-wrapper .swiper-slide').length,
+    lazyLoading: true,
+    autoplay: false,
+    speed: 600,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  setTimeout(function(){
+    galleryTop.params.autoplay = { delay: 4000 }
+    galleryTop.autoplay.start();
+  }, 4000);
+
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    loopedSlides: $('.gallery-thumbs .swiper-wrapper .swiper-slide').length,
+    speed: 600,
+    touchRatio: 0.2,
+    lazyLoading: true,
+    slideToClickedSlide: true,
+  });
+
+  // var galleryThumbs = new Swiper('.gallery-thumbs', {
+  //   spaceBetween: 10,
+  //   centeredSlides: true,
+  //   slidesPerView: 'auto',
+  //   loop: true,
+  //   touchRatio: 0.2,
+  //   slideToClickedSlide: true,
+  //   autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
+  galleryTop.controller.control = galleryThumbs;
+  galleryThumbs.controller.control = galleryTop;
+});
